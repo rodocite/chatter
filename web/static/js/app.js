462a8349c96e51ba561ab6070f3f8af2b9d4eb2b
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-23 04:19:31 -0700

    Render User List

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -9,3 +9,48 @@ const socket = new Socket('/socket', {
 })
 
 socket.connect()
+
+// User List
+let presences = {}
+
+const formatTimestamp = timestamp => {
+  const date = new Date(timestamp)
+
+  return date.toLocaleTimeString()
+}
+
+const listBy = (user, {metas: metas}) => {
+  return {
+    user: user,
+    onlineAt: formatTimestamp(metas[0].online_at)
+  }
+}
+
+const userList = document.getElementById('UserList')
+
+const render = presences => {
+  userList.innerHTML = Presence.list(presences, listBy)
+    .map(presence => `
+      <li>
+        ${presence.user}
+        <br>
+        <small>online since ${presence.onlineAt}</small>
+      </li>
+    `)
+    .join('')
+  }
+
+  // Channels
+  const room = socket.channel('room:lobby')
+  
+  room.on('presence_state', state => {
+    presences = Presence.syncState(presences, state)
+    render(presences)
+  })
+
+  room.on('presence_diff', diff => {
+    presences = Presence.syncDiff(presences, diff)
+    render(presences)
+  })
+
+  room.join()
