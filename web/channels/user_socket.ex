77f8159da39605cdd728a403134f93b12e256cb8
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-23 03:57:37 -0700

    Enable Phoenix channels for socket connections

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Chatter.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Chatter.RoomChannel
+  channel "room:*", Chatter.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
