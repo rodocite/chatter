edfd9b30897df507413a08d6e043eae985978f6f
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-23 04:44:42 -0700

    Only allow messages that have actual content

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -59,7 +59,7 @@ const render = presences => {
   const messageInput = document.getElementById('NewMessage')
   
   messageInput.addEventListener('keypress', e => {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 && messageInput.value) {
       room.push('message:new', messageInput.value)
       messageInput.value = ''
     }
